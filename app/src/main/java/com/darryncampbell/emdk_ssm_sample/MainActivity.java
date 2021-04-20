cb9fa78d03c2b417108beb53d0faab71550aa03f
darryncampbell <darryncampbell@hotmail.com>
2021-04-20 16:36:00 +0100

    Update / query / insert / delete

--- app/src/main/java/com/darryncampbell/emdk_ssm_sample/MainActivity.java
+++ app/src/main/java/com/darryncampbell/emdk_ssm_sample/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.content.pm.SigningInfo;
 import android.database.ContentObserver;
+import android.database.Cursor;
 import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,7 +16,10 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
@@ -24,35 +28,41 @@ import org.apache.commons.codec.binary.Hex;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //  todo Remove contentobserver and datasetobserver
-    //  todo implement Insert, Update, Clear and Query logic
     //  todo Logging (with app name)
+    //  todo Encryption of inserted data
+    //  todo create ER barcode for repository
+    //  todo Remove other todos & tidy code.
     //  todo Update UI to match Zebra standards
     //  todo create pre-compiled app with different name (new branch) signed with key added to this app, to demo how to share between apps
     //  todo Improve commenting
     //  todo Update Readme
 
     private static final String AUTHORITY = "content://com.zebra.securestoragemanager.securecontentprovider/data";
+    private static final String COLUMN_ORIG_APP_PACKAGE = "orig_app_package";
     private static final String COLUMN_TARGET_APP_PACKAGE = "target_app_package";
-    private static final String COLUMN_KEY = "data_name";
+    private static final String COLUMN_DATA_NAME = "data_name";
     private static final String COLUMN_DATA_VALUE = "data_value";
     private static final String COLUMN_DATA_INPUT_FORM = "data_input_form";
     private static final String COLUMN_DATA_OUTPUT_FORM = "data_output_form";
-    private String COLUMN_DATA_PERSIST_REQUIRED = "data_persist_required";
-    private String COLUMN_MULTI_INSTANCE_REQUIRED = "multi_instance_required";
+    private static final String COLUMN_DATA_PERSIST_REQUIRED = "data_persist_required";
+    private static final String COLUMN_MULTI_INSTANCE_REQUIRED = "multi_instance_required";
 
     private String currentPackage = "";
     private String currentPackageSignature = "";
     Uri cpUri;
-    ContentProviderClient cpClient;
-    LocalContentObserver myContentObserver;
-    LocalDataSetObserver myDataSetObserver;
+    TextView txtName;
+    TextView txtValue;
     TextView dataStorageResult;
+    TextView queryResult;
+    Switch switchPersistence;
+    private final String LOG_TAG = "SSM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataStorageResult = findViewById(R.id.txtDataStorageResult);
+        queryResult = findViewById(R.id.txtQueryResult);
         TextView txtCurrentPackage = findViewById(R.id.txtCurrentPackage);
         currentPackage = getPackageName();
         currentPackageSignature = getCurrentPackageSignature();
@@ -60,46 +70,199 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView txtCurrentPackageSig = findViewById(R.id.txtCurrentPackageSig);
         txtCurrentPackageSig.setText(currentPackageSignature);
         cpUri = Uri.parse(AUTHORITY);
-        //  todo this line is causing E/ActivityThread: Failed to find provider info for com.zebra.securestoragemanager.securecontentprovider
-        cpClient = getContentResolver().acquireContentProviderClient(cpUri);
+        //  Check the content provider will resolve - useful for root causing issues.
+        ContentProviderClient cpClient = getContentResolver().acquireContentProviderClient(cpUri);
+        if (cpClient == null)
+        {
+            String message = "Unable to create content resolver, please check your manifest";
+            Log.e(LOG_TAG, message);
+            dataStorageResult.setText(message);
+        }
 
-        myContentObserver = new LocalContentObserver(null);
-        myDataSetObserver = new LocalDataSetObserver();
-
-        dataStorageResult = findViewById(R.id.txtDataStorageResult);
         Button btnInsert = findViewById(R.id.btnInsert);
         btnInsert.setOnClickListener(this);
-    }
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(this);
+        Button btnDelete = findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(this);
+        Button btnQuery = findViewById(R.id.btnQuery);
+        btnQuery.setOnClickListener(this);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        getContentResolver().registerContentObserver(cpUri, true, myContentObserver);
-    }
+        txtName = findViewById(R.id.editName);
+        txtValue = findViewById(R.id.editValue);
+        Switch switchInputDataFormat = findViewById(R.id.switchEncryptInput);
+        switchInputDataFormat.setEnabled(false);
+        Spinner spinnerOutputFormat = findViewById(R.id.spinnerOutputDataFormat);
+        spinnerOutputFormat.setEnabled(false);
+        switchPersistence = findViewById(R.id.switchPersistence);
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-//        getContentResolver().unregisterContentObserver(myContentObserver);
+
     }
 
     private void insert()
     {
-        String key = "the-key"; //  todo read from UI & validate
-        String value = "the-value"; //  todo read value from UI & validate
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_TARGET_APP_PACKAGE, getAuthorizedPackages());
-        values.put(COLUMN_KEY, key);
-        values.put(COLUMN_DATA_VALUE, value);
-        values.put(COLUMN_DATA_INPUT_FORM, "1"); //plaintext =1  //  todo read from UI
-        values.put(COLUMN_DATA_OUTPUT_FORM, "1"); //plaintext=1 //  todo read from UI
-        values.put(COLUMN_DATA_PERSIST_REQUIRED, "false");  //  todo read from UI
-        values.put(COLUMN_MULTI_INSTANCE_REQUIRED, "true"); //  todo read from UI (not implemented yet)
+        try
+        {
+            String key = txtName.getText().toString();
+            if (key.trim().equals(""))
+            {
+                dataStorageResult.setText("Name cannot be blank");
+                return;
+            }
+            String value = txtValue.getText().toString();
+            if (value.trim().equals(""))
+            {
+                dataStorageResult.setText("Value cannot be blank");
+                return;
+            }
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_TARGET_APP_PACKAGE, getAuthorizedPackages());
+            values.put(COLUMN_DATA_NAME, key);
+            values.put(COLUMN_DATA_VALUE, value);
+            String persistData = switchPersistence.isChecked() ? "true" : "false";
+            values.put(COLUMN_DATA_PERSIST_REQUIRED, persistData);
+            Switch switchInputDataFormat = findViewById(R.id.switchEncryptInput);
+            String inputDataFormat = switchInputDataFormat.isChecked() ? "2" : "1";
+            values.put(COLUMN_DATA_INPUT_FORM, inputDataFormat); //plain text =1
+            Spinner spinnerOutputFormat = findViewById(R.id.spinnerOutputDataFormat);
+            String outputFormat = "1"; //  plain text
+            switch (spinnerOutputFormat.getSelectedItemPosition())
+            {
+                case (1):
+                    outputFormat = "2"; //  Encrypted
+                    break;
+                case (2):
+                    outputFormat = "3"; //  Keystrokes
+                    break;
+            }
+            values.put(COLUMN_DATA_OUTPUT_FORM, outputFormat);
+            values.put(COLUMN_MULTI_INSTANCE_REQUIRED, "false"); //  todo read from UI (not implemented yet)
 
-        //  todo catch exception if add duplicate entry
-        Uri createdRow = getContentResolver().insert(cpUri, values);
-        dataStorageResult.setText(createdRow.toString());
+            Uri createdRow = getContentResolver().insert(cpUri, values);
+            String message = "Inserted item at: " + createdRow.toString();
+            dataStorageResult.setText(message);
+            Log.i(LOG_TAG, message);
+        }
+        catch (Exception e)
+        {
+            String message = "Error Inserting: " + e.getMessage();
+            Log.e(LOG_TAG, message);
+            dataStorageResult.setText(message);
+        }
     }
+
+    private void update()
+    {
+        //  https://developer.android.com/reference/android/content/ContentProvider#update(android.net.Uri,%20android.content.ContentValues,%20java.lang.String,%20java.lang.String[])
+        try {
+            String key = txtName.getText().toString();
+            if (key.trim().equals(""))
+            {
+                dataStorageResult.setText("Name cannot be blank");
+                return;
+            }
+            String value = txtValue.getText().toString();
+            if (value.trim().equals(""))
+            {
+                dataStorageResult.setText("Value cannot be blank");
+                return;
+            }
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_TARGET_APP_PACKAGE, getAuthorizedPackages());
+            values.put(COLUMN_DATA_NAME, key);
+            values.put(COLUMN_DATA_VALUE, value);
+            String persistData = switchPersistence.isChecked() ? "true" : "false";
+            values.put(COLUMN_DATA_PERSIST_REQUIRED, persistData);
+
+            int rowNumbers = getContentResolver().update(cpUri, values, null , null);
+            String message = "Records updated: " + rowNumbers;
+            Log.i(LOG_TAG, message);
+            dataStorageResult.setText(message);
+        }
+        catch (Exception e)
+        {
+            Log.e(LOG_TAG, "Error Updating: " + e.getMessage());
+        }
+
+    }
+
+    private void delete()
+    {
+        //  https://developer.android.com/reference/android/content/ContentProvider#delete(android.net.Uri,%20java.lang.String,%20java.lang.String[])
+        try{
+            Uri cpUriDelete = Uri.parse(AUTHORITY + "/[" + currentPackage + "]");
+            String whereClauseAll = null;
+            //  Other where clause examples:
+            String whereClauseNonPersistentOnly = COLUMN_TARGET_APP_PACKAGE + " = '" + currentPackage + "' AND " + COLUMN_DATA_PERSIST_REQUIRED + " = 'false'";
+            String whereClauseSpecificKey = COLUMN_TARGET_APP_PACKAGE + " = '" + currentPackage + "' AND " + COLUMN_DATA_NAME + " = 'key'";
+            //  todo How can we delete a specific value?
+            String whereClauseSpecificValue = COLUMN_TARGET_APP_PACKAGE + " = '" + currentPackage + "' AND " + COLUMN_DATA_VALUE + " = 'data'";
+            int rowsAffected = getContentResolver().delete(cpUriDelete, whereClauseAll , null);
+            dataStorageResult.setText("Deleted " + rowsAffected + " rows");
+        }catch(Exception e){
+            String message = "Delete - error: " + e.getMessage();
+            Log.e(LOG_TAG, message);
+            dataStorageResult.setText(message);
+        }
+    }
+
+    private void query()
+    {
+        //  https://developer.android.com/reference/android/content/ContentProvider#query(android.net.Uri,%20java.lang.String[],%20java.lang.String,%20java.lang.String[],%20java.lang.String)
+        Uri cpUriQuery = Uri.parse(AUTHORITY + "/[" + currentPackage + "]");
+        String persistData = switchPersistence.isChecked() ? "true" : "false";
+
+        //  todo question: Why do I NEED to specify the data_persist_required element here?  Otherwise the cursor query returns nothing.
+        String selection = COLUMN_TARGET_APP_PACKAGE + " = '" + currentPackage + "'" + "AND " + "data_persist_required = '" + persistData + "'";
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(cpUriQuery, null, selection, null, null);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Cursor Query Error: " + e.getMessage());
+        }
+
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                StringBuilder strBuild = new StringBuilder();
+                String queryResults = "Entries found: " + cursor.getCount() + "\n\n";
+                while (!cursor.isAfterLast()) {
+                    String record = "";
+                    record += "Original app: " + cursor.getString(cursor.getColumnIndex(COLUMN_ORIG_APP_PACKAGE)) + "\n";
+                    //  todo can you send the same data to multiple apps?
+                    record += "Target app: " + cursor.getString(cursor.getColumnIndex(COLUMN_ORIG_APP_PACKAGE)) + "\n";
+                    record += "Data Name : " + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_NAME)) + "\n";
+                    record += "Data Value: " + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_VALUE)) + "\n";
+                    record += "Input  form: " + convertInputForm(cursor.getString(cursor.getColumnIndex(COLUMN_DATA_INPUT_FORM))) + "\n";
+                    record += "Output form: " + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_OUTPUT_FORM)) + "\n";
+                    record += "Persistent?: " + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_PERSIST_REQUIRED)) + "\n";
+                    record += "\n";
+                    queryResults += record;
+                    cursor.moveToNext();
+                }
+                Log.d(LOG_TAG, "Query data: " + strBuild);
+                queryResult.setText(queryResults);
+            } else {
+                queryResult.setText("No Records Found");
+            }
+        } catch (Exception e) {
+            Log.d(LOG_TAG, "Query data error: " + e.getMessage());
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    private String convertInputForm(String inputForm)
+    {
+        if (inputForm.equals("1"))
+            return "plain text";
+        else if (inputForm.equals("2"))
+            return "encrypted";
+        else
+            return "unknown";
+    }
+
 
     /*
      * Get string as JSON array of target package name and base64 signature.
@@ -149,36 +312,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         if (view.getId() == R.id.btnInsert)
             insert();
-    }
-}
-
-class LocalContentObserver extends ContentObserver {
-    public LocalContentObserver(Handler handler) {
-        super(handler);
-    }
-
-    @Override
-    public void onChange(boolean selfChange) {
-        this.onChange(selfChange, null);
-    }
-
-    @Override
-    public void onChange(boolean selfChange, Uri uri) {
-    }
-}
-
-class LocalDataSetObserver extends DataSetObserver {
-    public LocalDataSetObserver() {
-
-    }
-
-    @Override
-    public void onInvalidated() {
-        super.onInvalidated();
-    }
-
-    @Override
-    public void onChanged() {
-        super.onChanged();
+        else if (view.getId() == R.id.btnUpdate)
+            update();
+        else if (view.getId() == R.id.btnDelete)
+            delete();
+        else if (view.getId() == R.id.btnQuery)
+            query();
     }
 }
